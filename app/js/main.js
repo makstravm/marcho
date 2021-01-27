52aaeab284700c0f5e7cffc55cf66039883411a8
makstravm <maks7-2-94@gmail.com>
2021-01-28 01:43:36 +0200

    added page shop

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,32 @@
 $(function () {
+  $('.shop-content__filter-btn').on('click', function () {
+    $('.shop-content__filter-btn').removeClass('shop-content__filter-btn--active');
+    $(this).addClass('shop-content__filter-btn--active');
+  });
+
+  $('.button-list').on('click', function () {
+    $('.product-item').addClass('product-item--list');
+  });
+
+  $('.button-grid').on('click', function () {
+    $('.product-item').removeClass('product-item--list');
+  });
+
+  $('.select-styler').styler();
+
+  $('.filter-price__input').ionRangeSlider({
+    type: "double",
+    onStart: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+    onChange: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+
+    }
+  });
+
   $('.top-slider__inner').slick({
     dots: true,
     arrows: false,
@@ -10,7 +38,7 @@ $(function () {
 
   });
   $(".star").rateYo({
-    rating: 3.6, 
+    rating: 3.6,
     starWidth: "17px",
     normalFill: "#ccccce",
     ratedFill: "#ffc35b",
@@ -23,7 +51,7 @@ $(function () {
     const minutes = Math.floor((total / 1000 / 60) % 60);
     const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
     const days = Math.floor(total / (1000 * 60 * 60 * 24));
-    
+
     return {
       total,
       days,
@@ -32,14 +60,14 @@ $(function () {
       seconds
     };
   }
-  
+
   function getTimeRemaining(endtime) {
     const total = Date.parse(endtime) - Date.parse(new Date());
     const seconds = Math.floor((total / 1000) % 60);
     const minutes = Math.floor((total / 1000 / 60) % 60);
     const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
     const days = Math.floor(total / (1000 * 60 * 60 * 24));
-    
+
     return {
       total,
       days,
@@ -48,32 +76,32 @@ $(function () {
       seconds
     };
   }
-  
+
   function initializeClock(className, endtime) {
     const clock = document.querySelector(className);
     const daysSpan = clock.querySelector('.promo__days');
     const hoursSpan = clock.querySelector('.promo__hours');
     const minutesSpan = clock.querySelector('.promo__minutes');
     const secondsSpan = clock.querySelector('.promo__seconds');
-  
+
     function updateClock() {
       const t = getTimeRemaining(endtime);
-  
+
       daysSpan.innerHTML = t.days;
       hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
       minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
       secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-  
+
       if (t.total <= 0) {
         clearInterval(timeinterval);
       }
     }
-  
+
     updateClock();
     const timeinterval = setInterval(updateClock, 1000);
   }
-  
-  const deadline =$('.promo__clock').attr('data-time');
+
+  const deadline = $('.promo__clock').attr('data-time');
   initializeClock('.promo__clock', deadline);
 
 
